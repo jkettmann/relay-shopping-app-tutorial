488cbfd41a9f192822bb30656643553bf803e7a1
Johannes Kettmann <jkettmann@gmx.net>
2017-04-22 09:05:47 +0200

    Use Relay data in product list

--- app/components/ProductList/index.js
+++ app/components/ProductList/index.js
@@ -1,22 +1,52 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Relay from 'react-relay'
 
 import ListItem from './ListItem'
 import ProductTeaser from './ProductTeaser'
 
 import styles from './styles.css'
 
-import products from './products.json'
-
-const ProductList = () => (
+const ProductList = ({ viewer }) => (
   <div className={styles.wrapper}>
     {
-      products.map(product => (
-        <ListItem key={product.id}>
-          <ProductTeaser {...product} />
+      viewer.allProducts.edges.map(product => (
+        <ListItem key={product.node.id}>
+          <ProductTeaser {...product.node} />
         </ListItem>
       ))
     }
   </div>
 )
 
-export default ProductList
+ProductList.propTypes = {
+  viewer: PropTypes.shape({
+    allProducts: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        node: PropTypes.object,
+      })).isRequired,
+    }).isRequired,
+  }).isRequired,
+}
+
+export default Relay.createContainer(ProductList, {
+  initialVariables: {
+    limit: 10,
+  },
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on Viewer {
+        allProducts(first: $limit) {
+          edges {
+            node {
+              id
+              name
+              imageUrl
+              price
+            }
+          }
+        }
+      }
+    `,
+  },
+})
